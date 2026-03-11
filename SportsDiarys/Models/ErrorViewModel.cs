ef948f68fa03f70775386b34a6c997de41cb2baf
PetqEva <petq.69@abv.bg>
2026-03-11 21:20:46 +0200

    My Application created

---
+++ SportsDiarys/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SportsDiarys.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
